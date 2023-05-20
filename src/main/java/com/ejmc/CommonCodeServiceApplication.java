cac2bb45ef45de587aef3f33e6967eafd94aa60d
sanghun92 <mue0329@gmail.com>
2023-05-20 18:39:33 +0900

    chore : 프로젝트 구성

---
+++ src/main/java/com/ejmc/CommonCodeServiceApplication.java
@@ -0,0 +1,13 @@
+package com.ejmc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CommonCodeServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CommonCodeServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ejmc/CommonCodeServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ejmc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CommonCodeServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
